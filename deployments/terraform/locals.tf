b890e07748d84169f8ded74a6c285dc39c6300da
Chris Dower <devwowow@gmail.com>
2021-09-11 23:52:55 -0700

    changes

--- deployments/terraform/locals.tf
+++ deployments/terraform/locals.tf
@@ -3,10 +3,22 @@ locals {
   deployments = {
     home-assistant-complexity = {
         namespace = "home",
-        channel   = "https://github.com/home-assistant/core/releases/latest"
-        container = "home-assistant"
-        deploy    = "home-assistant-complexity"
+        channel   = "https://github.com/home-assistant/core/releases/latest",
+        container = "home-assistant",
+        deploy    = "home-assistant-complexity",
     },
+    "ombi-complexity" = {
+      namespace = "home",
+      channel   = "https://github.com/Ombi-app/Ombi/releases/latest",
+      container = "ombi",
+      deploy    = "ombi-complexity",
+    },
+    "sonar-complexity" = {
+      namespace = "home",
+      channel   = "https://github.com/linuxserver/docker-sonarr/releases/latest",
+      container = "sonar",
+      deploy    = "sonar-complexity",
+    }
   }
   regcred_enable = (var.registry_user != "" && var.registry_pass != "") ? 1 : 0
 }
